f1e964ea4b8f0aa43b36ce64e8be771e82d6adaa
meshack34 <meshackkimutai34@gmail.com>
2025-08-01 19:04:11 +0300

    update admin file

--- Tour/admin.py
+++ Tour/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Booking,Itinerary
+from .models import Destination, DestinationImage, Room, Restaurant, Activity, Information
 
-
-
-# Register your models here.
-
-admin.site.register(Booking)
-admin.site.register(Itinerary)
+admin.site.register(Destination)
+admin.site.register(DestinationImage)
+admin.site.register(Room)
+admin.site.register(Restaurant)
+admin.site.register(Activity)
+admin.site.register(Information)
