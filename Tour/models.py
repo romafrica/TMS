e24f4d2fca5e01eb30c2d5d0a2a4499b3de92d85
meshack34 <meshackkimutai34@gmail.com>
2025-08-01 18:48:56 +0300

    updates

--- Tour/models.py
+++ Tour/models.py
@@ -1,47 +1,51 @@
-from django.contrib.auth.models import User
+#tour/models
 from django.db import models
-from datetime import date
+from django.db import models
+from django.contrib.auth.models import User
 
-
-class Booking(models.Model):
-    first_name=models.CharField(max_length=100,default='Unknown')
-    last_name=models.CharField(max_length=100,  default='Unknown')
-    email=models.EmailField(max_length=100, default='default@example.com')
-    phone=models.CharField(max_length=100,  default='0000000000')
-    Original_location=models.CharField(max_length=100, default='Unknown')
-    destination=models.CharField(max_length=100, default='Unknown')
-    departure_date=models.DateField(default='2025-01-01')
-    return_date=models.DateField(default='2025-01-01')
-    created_at= models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated_at= models.DateTimeField(auto_now=True, null=True, blank=True)
-
-    class Meta:
-        ordering= ['-updated_at', '-created_at']
-
-    def __str__(self):
-        return self.first_name + ' ' + self.last_name 
-    
-
-
-
-class Itinerary(models.Model):
-    first_name=models.ForeignKey(Booking, on_delete=models.CASCADE, null=True, blank=True)
-    location=models.CharField(max_length=100, default='Unknown')
-    activity=models.TextField()
-    cost=models.DecimalField(max_digits=10, decimal_places=2 ) 
-    date=models.DateField(default='2025-01-01')
-   # time=models.TimeField()  
-    created_at= models.DateTimeField(auto_now_add=True)
-    updated_at= models.DateTimeField(auto_now=True)
-    user=models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-
-    class Meta:
-        ordering= ['updated_at','-created_at']
-    
+# DESTINATION_CHOICES = [
+#         ('KISUMU', 'Kisumu'),
+#         ('MOMBASA', 'Mombasa'),
+#         ('NAIROBI', 'Nairobi'),
+       
+#     ]
+class Destination(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='destinations')
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    map_embed_code = models.TextField(help_text="Embed iframe from Google Maps")
 
     def __str__(self):
-        return self.location
+        return self.name
 
 
 
+class DestinationImage(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='destination_gallery/')
 
+class Room(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='rooms')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='rooms/')
+
+class Restaurant(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='restaurants')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='restaurants/')
+
+
+
+class Activity(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='activities')
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Information(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE, related_name='info')
+    content = models.TextField()
