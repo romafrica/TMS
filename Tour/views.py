448e179a387278c42d8f40dc7efc235b91113c64
meshack34 <meshackkimutai34@gmail.com>
2025-08-04 17:36:31 +0300

    update admin file

--- Tour/views.py
+++ Tour/views.py
@@ -2,12 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Booking,Itinerary
-from .forms import BookingForm,ItineraryForm
-from django.db.models import Q
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 
 def home(request,):
